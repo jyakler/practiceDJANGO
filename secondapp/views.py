27fa13ac15cf2b54830d44275b948388389418ba
jyakler <jyakler@gmail.com>
2022-02-18 17:44:08 +0900

    2-18

--- secondapp/views.py
+++ secondapp/views.py
@@ -167,3 +167,105 @@ def exam19(request) :
     dt = datetime.now()
     context = {'current_date' : dt}
     return render(request, 'exam19.html', context)
+
+from django.http import HttpResponse, JsonResponse
+
+def query_form_get(request):
+    return render(request, "queryinput.html", {"mymethod" : "GET"})
+
+def query_form_post(request):
+    return render(request, "queryinput.html", {"mymethod" : "POST"})
+
+def querystring_processing(request):
+    context = None
+    if request.method == 'POST':
+        queryinput = request.POST
+    else :
+        queryinput = request.GET
+    gname =  queryinput.get("guestname") if queryinput.get("guestname") != "" else "이름이 입력되지 않음"
+    ghobby = queryinput.getlist("guesthobby", [])
+    gcolor = queryinput.get("guestcolor", "yellow")
+    gmemo = queryinput.get("guestmemo") if queryinput.get("guestmemo") != "" else "입력된 메모가 없음"
+    context = {
+      'gname' : gname,
+      'ghobby': ghobby,
+      'gcolor': gcolor,
+      'gmemo': gmemo,
+    }
+    return render(request, 'queryoutput.html', context)
+
+
+def exam20(request):
+    return render(request, 'exam20.html')
+
+def exam21(request):
+    return render(request, 'exam21.html')
+
+def exam22(request):
+    return render(request, 'exam22.html')
+
+def exam23(request):
+    return render(request, 'exam23.html')
+
+def exam24(request):
+    return render(request, 'exam24.html')
+
+def exam25(request):
+    return render(request, 'exam25.html')
+
+def json1(request):
+    return JsonResponse({
+        'message' : '안녕 파이썬 장고',
+        'items' : ['가나다', '파이썬', '장고', '자바스크립트', 'CSS3'],
+        'num' : 100
+    }, json_dumps_params={'ensure_ascii':False})
+
+def json2(request):
+    return JsonResponse({ "time" : datetime.now().strftime('%H시 %M분 %S초') },
+                        json_dumps_params={'ensure_ascii':False})
+
+def ggmap1(request) :
+    return render(request, "ggmap1.html", None)
+
+def ggmap2(request) :
+    return render(request, "ggmap2.html", None)
+
+def ggmap3(request) :
+    return render(request, "ggmap3.html", None)
+
+def ggmap4(request) :
+    return render(request, "ggmap4.html", None)
+
+def ggmap5(request) :
+    return render(request, "ggmap5.html", None)
+
+def ggmap6(request) :
+    lat = [37.5115, 37.5094, 37.5080, 37.5110, 37.5088]
+    lng = [127.0500, 127.0503, 127.0600, 127.0590, 127.0560];
+    hname = ['듀크1', '듀크2', '듀크3', '듀크4', '듀크5'];
+    address = ['aaa', 'bbb', 'ccc', 'ddd', 'eee'];
+    context = {
+                'lat' : lat, 'lng' : lng, 'hname' : hname, 'address' : address
+    }
+    return render(request, "ggmap6.html", context)
+
+def kkmap1(request) :
+    return render(request, "kkmap1.html", None)
+
+def kkmap2(request) :
+    return render(request, "kkmap2.html", None)
+
+def kkmap3(request) :
+    return render(request, "kkmap3.html", None)
+
+def kkmap4(request) :
+    return render(request, "kkmap4.html", None)
+
+def kkmap5(request) :
+    return render(request, "kkmap5.html", None)
+
+def kkmap6(request) :
+    return render(request, "kkmap6.html", None)
+
+def kkmap7(request) :
+    return render(request, "kkmap7.html", None)
